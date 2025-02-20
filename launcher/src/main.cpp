0e4c92c3a3aca0698de5a70dff75f00904d58af8
Ian Pike <Ianpike98@gmail.com>
2025-02-19 19:53:58 -0500

    Init commit

---
+++ launcher/src/main.cpp
@@ -0,0 +1,60 @@
+#include "fornani/core/CrashHandler.hpp"
+#include "fornani/io/PathHelpers.hpp"
+#include "launcher/LauncherApplication.hpp"
+
+#include <steam/steam_api.h>
+
+#include "cpptrace/from_current.hpp"
+#include "libassert/assert.hpp"
+
+#ifndef FORNANI_STEAM_APP_ID
+#error "FORNANI_STEAM_APP_ID was not defined!"
+#endif
+
+#if defined(FORNANI_STEAM_APP_ID) && FORNANI_STEAM_APP_ID < 0
+#error "FORNANI_STEAM_APP_ID was defined as a negative number!"
+#endif
+
+static constexpr char const* logFile{"fornani.log"};
+
+int main(int argc, char** argv) {
+	DEBUG_ASSERT(argc > 0); // TODO: Get rid of this.
+
+	// TODO: Maybe move this into a config file?
+	auto config = fornani::io::logger::Config{};
+	// Required to initialize the logger for the application. This must also stay outside any try/catch block.
+	auto log_instance = fornani::io::logger::Instance{logFile, config};
+	fornani::io::Logger const main_logger{"Main"};
+
+	// TODO: Replace usage of argv[0] with new method to get the resource dir.
+	auto exe_dir = fornani::io::abs_exe_directory();
+
+	launcher::LauncherApplication app{argv};
+	app.init(argv);
+
+	constexpr auto steam_id = FORNANI_STEAM_APP_ID;
+	NANI_LOG_INFO(main_logger, "Current passed steam ID: {}", steam_id);
+
+	if (SteamAPI_RestartAppIfNecessary(steam_id)) {
+		NANI_LOG_INFO(main_logger, "Steam requested we re-launch through Steam.");
+		return EXIT_SUCCESS;
+	}
+	SteamErrMsg errMsg;
+	if (SteamAPI_InitEx(&errMsg) != k_ESteamAPIInitResult_OK) {
+		NANI_LOG_ERROR(main_logger, "Failed to init Steam: {}", static_cast<char const*>(errMsg));
+		return EXIT_FAILURE;
+	}
+
+	NANI_LOG_INFO(main_logger, "SteamAPI has been initialized.");
+
+	CPPTRACE_TRY { app.launch(argv); }
+	CPPTRACE_CATCH(std::exception const& e) {
+		NANI_LOG_ERROR(main_logger,
+					   "Exception: {}\n"
+					   "Trace:\n"
+					   "{}\n",
+					   e.what(), cpptrace::from_current_exception().to_string());
+	}
+
+	return EXIT_SUCCESS;
+}
